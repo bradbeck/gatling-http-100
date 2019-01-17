f5514e0ab9d3846d0841602f9fec5b4547ea2381
Brad Beck <bradley.beck@gmail.com>
2019-01-17 09:52:57 -0600

    refine

--- src/test/scala/com/example/MavenSimulation.scala
+++ src/test/scala/com/example/MavenSimulation.scala
@@ -4,22 +4,25 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import org.testcontainers.containers.GenericContainer
 
-import scala.concurrent.duration._
-
-class MavenSimulation extends Simulation
+class MavenSimulation
+    extends Simulation
 {
-  class GContainer(image: String) extends GenericContainer[GContainer] {
-    setDockerImageName(image)
-  }
+
+  class GContainer(image: String)
+      extends GenericContainer[GContainer](image)
 
   val alpine: GContainer = new GContainer("crccheck/hello-world").withExposedPorts(8000)
+
   alpine.start()
-  val url: String = "http://" + alpine.getContainerIpAddress + ":" + alpine.getMappedPort(8000)
+
+  val url: String = s"http://${alpine.getContainerIpAddress}:${alpine.getMappedPort(8000)}/"
+
   val baseProtocol = http
-          .disableWarmUp
-          .userAgentHeader("Gatling")
-          .connectionHeader("Close")
-          .acceptEncodingHeader("gzip,deflate")
+      .disableWarmUp
+      .userAgentHeader("Gatling")
+      .connectionHeader("Close")
+      .acceptEncodingHeader("gzip,deflate")
+
   val scn = scenario("Simple GET")
       .exec(http("get hello")
           .get(url)
@@ -28,11 +31,15 @@ class MavenSimulation extends Simulation
           )
       )
 
-  setUp (
+  setUp(
     scn.inject(
-      constantConcurrentUsers(4) during (30 seconds)
+      atOnceUsers(10)
     )
-  ).protocols(baseProtocol)
+  )
+      .assertions(
+        details("get hello").successfulRequests.percent.gt(99)
+      )
+      .protocols(baseProtocol)
 
   after {
     alpine.stop()
